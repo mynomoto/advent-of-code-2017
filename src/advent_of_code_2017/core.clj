574c547cdadc0aaf05c4fbe7a2814faae46ccd16
mynomoto <mynomoto@gmail.com>
2018-12-02 22:59:56 -0200

    Day 11

--- src/advent_of_code_2017/core.clj
+++ src/advent_of_code_2017/core.clj
@@ -1,7 +1,45 @@
 (ns advent-of-code-2017.core
-  (:gen-class))
+  (:gen-class)
+  (:require [clojure.java.io :as io]
+            [clojure.string :as str]))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
   (println "Hello, World!"))
+
+(def direction->coordinate-change
+  {"n" [0 +1 -1],
+   "nw" [-1 +1 0],
+   "ne" [+1 0 -1],
+   "sw" [-1 0 +1],
+   "se" [+1 -1 0],
+   "s" [0 -1 +1]})
+
+(def day11-input (slurp (io/resource "day11-input")))
+
+(defn abs [n] (if (pos? n) n (- n)))
+
+(defn day11-part1
+  [day11-input]
+  (->> (str/split (str/trim day11-input) #",")
+       (reduce (fn [current movement]
+                 (let [[x y z] current
+                       [dx dy dz] (direction->coordinate-change movement)]
+                   [(+ x dx) (+ y dy) (+ z dz)]))
+         [0 0 0])
+       (map abs)
+       (apply max)))
+
+(defn day11-part2
+  []
+  (->> (str/split (str/trim day11-input) #",")
+       (reductions (fn [current movement]
+                     (let [[x y z] current
+                           [dx dy dz] (direction->coordinate-change movement)]
+                       [(+ x dx) (+ y dy) (+ z dz)]))
+                   [0 0 0])
+       (map #(->> %
+                  (map abs)
+                  (reduce max)))
+       (reduce max)))
